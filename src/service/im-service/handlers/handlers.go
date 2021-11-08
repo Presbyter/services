d8a65d9b910ba0f1e775cdaa558a4bc127b0e62b
XingChao <xingchao0203@hotmail.com>
2021-11-08 19:03:53 +0800

    create im token

--- src/service/im-service/handlers/handlers.go
+++ src/service/im-service/handlers/handlers.go
@@ -2,7 +2,12 @@ package handlers
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"net/url"
+	"strconv"
 
 	pb "github.com/Presbyter/services/protobuf"
 )
@@ -22,6 +27,27 @@ func (s ntesIMService) CreateAccount(ctx context.Context, in *pb.CreateAccountRe
 	var resp pb.CreateAccountResp
 	addr := ntesBaseUrl + "/nimserver/user/create.action"
 	data := url.Values{}
+	httpResp, err := http.PostForm(addr, data)
+	if err != nil {
+		return nil, err
+	}
+	defer httpResp.Body.Close()
+	body, err := io.ReadAll(httpResp.Body)
+	if err != nil {
+		return nil, err
+	}
+	m := make(map[string]interface{})
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		return nil, err
+	}
+	if val, ok := m["code"].(float64); ok && val != 200 {
+		return nil, fmt.Errorf(strconv.Itoa(int(val)))
+	}
+
+	resp.Accid = m["info"].(map[string]interface{})["accid"].(string)
+	resp.Token = m["info"].(map[string]interface{})["token"].(string)
+	resp.Name = m["info"].(map[string]interface{})["name"].(string)
 	return &resp, nil
 }
 
